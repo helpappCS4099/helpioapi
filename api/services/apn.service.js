03104ce81e3a146288fa656603d6cdc27702661a
artemrakhmanov <64369654+artemrakhmanov@users.noreply.github.com>
2023-04-06 19:16:10 +0100

    test comments added

--- api/services/apn.service.js
+++ api/services/apn.service.js
@@ -1,6 +1,12 @@
 const {getUserByID} = require("./user.service")
 const Errors = require("../utility/errors")
 
+/**
+ * Hey, this is a comment.
+ * @async
+ * @param {*} userID 
+ * @param {*} token 
+ */
 exports.setAPNToken = async (userID, token) => {
     const user = await getUserByID(userID)
     if (user === null) {
